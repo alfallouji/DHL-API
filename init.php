368e0e8512f560f0b852343184ddf7514db5ae6e
Bashar <bashar.al-fallouji@ssense.com>
2014-07-27 08:44:49 +1000

    Added header

--- init.php
+++ init.php
@@ -1,10 +1,28 @@
 <?php
 /**
+ * Note : Code is released under the GNU LGPL
+ *
+ * Please do not change the header of this file
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ */
+
+/**
+ * File:        init.php
+ * Project:     DHL API
+ *
+ * @author      Al-Fallouji Bashar
+ * @version     0.1
+ *
  * Initialize API (autloader and configuration file)
- *
  * Simply include this file in order to use the DHL API
- *
- * @project DHL-API
  */
 define('DHL_API_DIR', __DIR__ . '/');
 require_once(DHL_API_DIR . 'vendor/autoloadManager/autoloadManager.php');
