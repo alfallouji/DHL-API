a74cfce10ec62bad197fb4c2e37dea47b74ba0c2
Bashar <bashar.al-fallouji@ssense.com>
2014-07-27 08:39:27 +1000

    Added header

--- DHL/Entity/GB/ShipmentRequest.php
+++ DHL/Entity/GB/ShipmentRequest.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * Note : Code is released under the GNU LGPL
+ *
+ * Please do not change the header of this file
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ */
+
+/**
+ * File:        ShipmentRequest.php
+ * Project:     DHL API
+ *
+ * @author      Al-Fallouji Bashar
+ * @version     0.1
+ */
+
 namespace DHL\Entity\GB; 
 use DHL\Entity\Base;
 
@@ -23,7 +46,7 @@ class ShipmentRequest extends Base
      * @var string
      * Service XSD
      */
-    protected $_serviceXSD = 'ship-val-global-req.xsd';
+    protected $_serviceXSD = 'ShipmentRequest.xsd';
 
     /**
      * Parameters to be send in the body
