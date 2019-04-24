ec017178eb4a3269e89cd3d620f944a05d409de8
Martins Fridenbergs <martins.fridenbergs@mtcmedia.co.uk>
2019-04-24 14:56:14 +0300

    Line endings to LF and shipment update based on DHL feedback

--- src/Entity/GB/ShipmentRequest.php
+++ src/Entity/GB/ShipmentRequest.php
@@ -36,6 +36,28 @@ class ShipmentRequest extends Base
     protected $display_schema_version = true;
 
     /**
+     * @var array
+     */
+    protected $header_meta_params = [
+        'SoftwareName' => [
+            'type' => 'string',
+            'required' => true,
+            'subobject' => false,
+        ],
+        'SoftwareVersion' => [
+            'type' => 'string',
+            'required' => true,
+            'subobject' => false,
+        ],
+    ];
+
+    /**
+     * @var string
+     * The schema version
+     */
+    protected $schema_version = '6.2';
+
+    /**
      * Parameters to be send in the body
      * @var array
      */
